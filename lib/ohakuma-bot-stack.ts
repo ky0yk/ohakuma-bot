7007249babf741c078a15727b709c14be8bb934b
ky0yk <ksaik1024@gmail.com>
2021-07-11 09:03:46 +0900

    LambdaとAPIGWを定義

--- lib/ohakuma-bot-stack.ts
+++ lib/ohakuma-bot-stack.ts
@@ -1,11 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 import * as apigateway from '@aws-cdk/aws-apigateway';
+import { handler } from '../src/lambda/handlers/app';
 
 export class OhakumaBotStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const appLambda = new NodejsFunction(this, 'appLambda', {
+      entry: 'src/lambda/handlers/app.ts',
+      handler: 'handler',
+      environment: {
+        SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN || '',
+        SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET || '',
+      },
+    });
+
+    new apigateway.LambdaRestApi(this, 'slackApi', {
+      handler: appLambda,
+    });
   }
 }
